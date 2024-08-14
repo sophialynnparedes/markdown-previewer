91dfff8f39b0a8224a9cb0a3d1e16c1f054c61c6
sophialynnparedes <74270737+sophialynnparedes@users.noreply.github.com>
2024-08-14 12:43:34 -0500

    done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { marked } from "marked";
+import "./styles.scss";
+{
+    /*
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+User Story #4: When I enter GitHub flavored markdown into the #editor element, 
+the text is rendered as HTML in the #preview element as I type 
+(HINT: You don't need to parse Markdown yourself - you can import the Marked library for this: https://cdnjs.com/libraries/marked).
+
+User Story #5: When my markdown previewer first loads, the default text in the #editor field should contain 
+valid markdown that represents at least one of each of the following elements: a heading element (H1 size), 
+a sub heading element (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+
+User Story #6: When my markdown previewer first loads, 
+the default markdown in the #editor field should be rendered as HTML in the #preview element.
+*/
 }
+export default function App() {
+    const [text, setText] = useState("");
 
-export default App
+    marked.use({ breaks: true });
+
+    useEffect(() => {
+        const previewText = document.getElementById("preview");
+        previewText.innerHTML = marked.parse(text);
+    }, [text]);
+
+    return (
+        <>
+            <div id="editor-container">
+                <h1>Editor:</h1>
+                <textarea
+                    id="editor"
+                    onChange={(e) => {
+                        setText(e.target.value);
+                    }}></textarea>
+            </div>
+            <div id="preview-container">
+                <h1>Preview:</h1>
+                <div id="preview"></div>
+            </div>
+        </>
+    );
+}
